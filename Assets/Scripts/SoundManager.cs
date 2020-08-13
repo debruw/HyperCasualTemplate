d0132186e30e00ff1e89882ee37768fc856d5ab3
Ebru Çakar <ebru_cakar_11@hotmail.com>
2020-08-13 19:41:12 +0300

    Initial commit

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager _instance;
+
+    public static SoundManager Instance { get { return _instance; } }
+
+    public AudioSource[] soundsList;
+
+    //Sound Enums
+    public enum Sounds
+    {
+        Music
+    }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+        if (!PlayerPrefs.HasKey("SOUND"))
+        {
+            PlayerPrefs.SetInt("SOUND", 1);
+        }
+        if (!PlayerPrefs.HasKey("VIBRATION"))
+        {
+            PlayerPrefs.SetInt("VIBRATION", 1);
+        }
+    }
+
+
+    public void playSound(Sounds soundType)
+    {
+        if (PlayerPrefs.GetInt("SOUND") == 1)
+        {
+            soundsList[(int)soundType].Play();
+        }
+    }
+
+    public void stopSound(Sounds soundType)
+    {
+        soundsList[(int)soundType].Stop();
+    }
+
+
+}
